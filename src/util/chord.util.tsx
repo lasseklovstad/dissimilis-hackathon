41aa6c201d0481b330956a0ddf779e4297d0c487
lklovs <lasse.klovstad@gmail.com>
2020-06-09 15:25:38 +0200

    Akkorder til noter

--- src/util/chord.util.tsx
+++ src/util/chord.util.tsx
@@ -1,4 +1,5 @@
 import {AbcNotation, Chord as TonalChord} from "@tonaljs/tonal"
+import {noteRegex} from "./note.util"
 
 export const getNotes = (value: string) => {
     const chord = TonalChord.get(value)
@@ -8,5 +9,15 @@ export const getNotes = (value: string) => {
         })
     } else {
         return undefined
+    }
+}
+
+export const getChord = (abcNotes: string | undefined) => {
+    if (!abcNotes) return ''
+    const notes = abcNotes.match(noteRegex)?.map(note => note.replace(/[/\d*]/g, '')).map(note => AbcNotation.abcToScientificNotation(note))
+    if (notes && notes.length > 1) {
+        return TonalChord.detect(notes)[0]
+    } else {
+        return notes && notes[0].replace(/[\d*]/g, '')
     }
 }
--- src/util/note.util.tsx
+++ src/util/note.util.tsx
@@ -1,62 +1,36 @@
+import {AbcNotation} from "@tonaljs/tonal"
+
+const DISSIMILLIS_NOTES = ['C', '4', 'D', '5', 'E', 'F', '1', 'G', '2', 'A', '3', 'B']
+
 export const getNoteText = (note: string) => {
-    switch (note.replace('_', '').replace('^', '')) {
-        case "C":
-            if (note.indexOf('^') > -1) {
-                return '4'
-            } else if (note.indexOf('_') > -1) {
-                return 'H'
+    const match = noteRegex.exec(note)
+    noteRegex.exec(note)
+    if (match) {
+        const prefix = match[1]
+        const letter = match[2]
+        if (prefix) {
+            const basePosition = DISSIMILLIS_NOTES.indexOf(letter.toUpperCase())
+            const down = prefix.indexOf('_') > -1
+            let finalPosition
+            if (down) {
+                finalPosition = basePosition - prefix.length
+                if (finalPosition < 0) {
+                    finalPosition = finalPosition + 11
+                }
             } else {
-                return note
+                finalPosition = basePosition + prefix.length
+                if (finalPosition > 11) {
+                    finalPosition = finalPosition - 11
+                }
             }
-        case "D":
-            if (note.indexOf('^') > -1) {
-                return '5'
-            } else if (note.indexOf('_') > -1) {
-                return '4'
-            } else {
-                return note
-            }
-        case "E":
-            if (note.indexOf('^') > -1) {
-                return 'F'
-            } else if (note.indexOf('_') > -1) {
-                return '4'
-            } else {
-                return note
-            }
-        case "F":
-            if (note.indexOf('^') > -1) {
-                return '1'
-            } else if (note.indexOf('_') > -1) {
-                return 'E'
-            } else {
-                return note
-            }
-        case "G":
-            if (note.indexOf('^') > -1) {
-                return '2'
-            } else if (note.indexOf('_') > -1) {
-                return '1'
-            } else {
-                return note
-            }
-        case "A":
-            if (note.indexOf('^') > -1) {
-                return '3'
-            } else if (note.indexOf('_') > -1) {
-                return '2'
-            } else {
-                return note
-            }
-        case "B":
-            if (note.indexOf('^') > -1) {
-                return 'C'
-            } else if (note.indexOf('_') > -1) {
-                return '3'
-            } else {
-                return note
-            }
+            return DISSIMILLIS_NOTES[finalPosition]
+
+        } else {
+            return letter.toUpperCase() === 'Z' ? '-':letter.toUpperCase()
+        }
     }
+
+
 }
 
 export const COLOR = {
@@ -66,67 +40,46 @@ export const COLOR = {
     f: '#fffc38',
     g: '#fb1826',
     a: '#3443e7',
-    h: '#fd8c2f',
+    b: '#fd8c2f',
     default: '#3C3C3C'
 }
 
 export const getColor = (note: string) => {
-    switch (note.replace('_', '').replace('^', '')) {
-        case "C":
-            if (note.indexOf('^') > -1) {
-                return COLOR.default
-            } else if (note.indexOf('_') > -1) {
-                return COLOR.h
-            } else {
-                return COLOR.c
-            }
-        case "D":
-            if (note.indexOf('^') > -1) {
-                return COLOR.default
-            } else if (note.indexOf('_') > -1) {
-                return COLOR.default
-            } else {
-                return COLOR.d
-            }
-        case "E":
-            if (note.indexOf('^') > -1) {
-                return COLOR.f
-            } else if (note.indexOf('_') > -1) {
-                return COLOR.default
-            } else {
-                return COLOR.e
-            }
-        case "F":
-            if (note.indexOf('^') > -1) {
-                return COLOR.default
-            } else if (note.indexOf('_') > -1) {
-                return COLOR.e
-            } else {
-                return COLOR.f
-            }
-        case "G":
-            if (note.indexOf('^') > -1) {
-                return COLOR.default
-            } else if (note.indexOf('_') > -1) {
-                return COLOR.default
-            } else {
-                return COLOR.g
-            }
-        case "A":
-            if (note.indexOf('^') > -1) {
-                return COLOR.default
-            } else if (note.indexOf('_') > -1) {
-                return COLOR.default
-            } else {
-                return COLOR.a
-            }
-        case "B":
-            if (note.indexOf('^') > -1) {
-                return COLOR.c
-            } else if (note.indexOf('_') > -1) {
-                return COLOR.default
-            } else {
-                return COLOR.h
-            }
+    const letter = getNoteText(note)
+    if (!isNaN(letter as any)) {
+        return COLOR.default
+    } else if (letter) {
+        const note = letter.toLowerCase() as 'c' | 'd' | 'e' | 'f' | 'g' | 'a' | 'b'
+        return COLOR[note]
     }
+}
+
+export const noteRegex = /([_^]*)([A-Za-z])(\/)?(\d+)?/g
+// const chordRegex = /\[(([_^]*)([A-Za-z])(\/)?(\d+)?)+\]/g
+export const noteAndChordRegex = /((\[(([_^]*[A-Za-z]\/?\d*?)+)\])|(([_^]*)([A-Za-z])(\/)?(\d+)?))/g
+export const barLineRegex = /([:\[]?\|[:\d\]]?)/g
+
+export const getNotesLength = (note: string) => {
+    let length = 4
+    let match = noteRegex.exec(note)
+    noteRegex.exec(note)
+    if (match) {
+        const seperator = match[3]
+        const noteLengthValue = match[4]
+        if (seperator) {
+            const value = noteLengthValue ? parseInt(noteLengthValue) : 2
+            return length / value
+        } else {
+            const value = noteLengthValue ? parseInt(noteLengthValue) : 1
+            return length * value
+        }
+    }
+
+    return length
+
+
+}
+
+export const getNote = (note:string)=>{
+    return AbcNotation.scientificToAbcNotation(note + '4')
 }
