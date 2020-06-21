6e2f9890a40c3abee54a7a4fd7d49c91d21f0336
Lasse <lasse.klovstad@ciber.no>
2020-06-21 18:23:23 +0200

    Revert "Akkorder til noter"
    This reverts commit 80d3a45d

--- src/components/barLine/BarLine.component.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export type BarLineProps = {
-}
-
-export const BarLine : React.FC<BarLineProps> = props =>{
-    return <div style={{backgroundColor:'black', marginLeft:'10px',marginRight:'10px', height:'100px', width: '5px'}}>. </div>
-}
