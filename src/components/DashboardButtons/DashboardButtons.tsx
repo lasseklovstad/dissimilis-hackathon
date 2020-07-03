38e59f2c1eeb5f1ddb9e0f94a656cd1e4958f041
Max Torre Schau <max.torre.schau@ciber.no>
2020-07-03 13:26:26 +0200

    Small hack

--- src/components/DashboardButtons/DashboardButtons.tsx
+++ src/components/DashboardButtons/DashboardButtons.tsx
@@ -8,30 +8,33 @@ import { colors } from '../../utils/colors';
 
 type ButtonProps = {
   text: string,
-  link: string
+  link: string,
+  func? : Function,
+  color? : string,
 };
 
-export const DashboardButtonWithAddIcon: FunctionComponent<ButtonProps> = ({text, link}) => {
+export const DashboardButtonWithAddIcon: FunctionComponent<ButtonProps> = (props) => {
   const styles = useStyles();
+ 
   return (
       <Card className={styles.button}>
-        <CardActionArea to={link} component={Link}>
-          <Box className={styles.container}>
+        <CardActionArea onClick={() => props.func && props.func()} >
+          <Box className={styles.container} >
             <Box p={2}><AddIcon /></Box>
-            <Box p={2}><Typography>{text}</Typography></Box>
+            <Box p={2}><Typography>{props.text}</Typography></Box>
           </Box>
         </CardActionArea>
       </Card>
   );
 }
 
-export const DashboardButton: FunctionComponent<ButtonProps> = ({text, link}) => {
+export const DashboardButton: FunctionComponent<ButtonProps> = (props) => {
   const styles = useStyles();
   return (
       <Card className={styles.button}>
-        <CardActionArea to={link} component={Link}>
-          <Box className={styles.container}>
-            <Box p={2}><Typography>{text}</Typography></Box>
+        <CardActionArea to={props.link} component={Link}>
+          <Box className={styles.container} style={{backgroundColor: props.color}}>
+            <Box p={2}><Typography>{props.text}</Typography></Box>
           </Box>
         </CardActionArea>
       </Card>
