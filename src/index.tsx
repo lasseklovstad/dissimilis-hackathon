0b6f056eca23ff9369f70d3b5ea3bef616a12935
Martin Johannes Nilsen <martin.johannes.nilsen@ciber.no>
2020-07-09 10:28:43 +0200

    deploy #152

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+import './web.config';
 
 import App from './App'
 import "./i18n";
 
 ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
