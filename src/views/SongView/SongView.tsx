3ccb7a85f6b8373325f6ce9db9e20894c2aeb2dd
Max Torre Schau <max.torre.schau@ciber.no>
2020-07-09 16:09:00 +0200

    Fikset bug i SongView

--- src/views/SongView/SongView.tsx
+++ src/views/SongView/SongView.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavBarCreateSong from '../../components/NavBarCreateSong/NavBarCreateSong';
 import CreateSongTab from '../../components/CreateSongTab/CreateSongTab';
 import { Grid, makeStyles } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+
 
 export type SongViewProps = {
 
 }
 export const SongView: React.FC<SongViewProps> = props => {
   const classes = useStyles();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (localStorage.getItem("measure") === null) history.push({ pathname: "/dashboard" })
+  }, [history]);
 
 
   return (
