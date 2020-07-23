1915ba4d1a411d4298e091c0f8e3ccedd6183fbc
Johanna Haarseth <Johanna.Haarseth@ciber.no>
2020-07-23 13:58:04 +0200

    Begynt å legge til sang

--- src/utils/useGetSong.tsx
+++ src/utils/useGetSong.tsx
@@ -8,7 +8,9 @@ import { ISong } from "../models/ISong";
 export const useGetSong = (id: number) => {
     const url = 'songs/';
     const params = { 'Id': id.toString() };
-    const initialData: ISong[] = [];
-    const getSongs = useApiService<ISong[]>("get", url, { params, initialData }).fetchData;
-    return getSongs;
+    const apiKey = sessionStorage.getItem("apiKey") || "";
+    const userId = sessionStorage.getItem("userId") || "";
+    const headers = { 'X-API-Key': apiKey, 'X-User-ID': userId, };
+    const getSong = useApiService<ISong[]>("get", url, { params, headers }).fetchData;
+    return getSong;
 }
