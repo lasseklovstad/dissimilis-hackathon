6488fa09684a3b3b645560764b3443bebe0b9a71
Johanna Haarseth <Johanna.Haarseth@ciber.no>
2020-07-23 12:51:15 +0200

    Bgeynt å legge til modal på dashboard

--- src/utils/useGetRecentSongs.tsx
+++ src/utils/useGetRecentSongs.tsx
@@ -5,9 +5,12 @@ import { ISong } from "../models/ISong";
  * Get songs based on recent songs 
  **/
 export const useGetRecentSongs = () => {
-    const url = "Song/songs";
-    const params = { "Num": "5", "ArrangerId": "1", "OrderByDateTime": "true" };
+    const url = "song/search";
+    const params = { "Num": "5", "OrderByDateTime": "true" };
     const initialData: ISong[] = [];
-    const [dataFromApi] = useApiService<ISong[]>("get", url, { params, initialData });
-    return dataFromApi;
+    const apiKey = sessionStorage.getItem("apiKey") || "";
+    const userId = sessionStorage.getItem("userId") || "";
+    const headers = { 'X-API-Key': apiKey, 'X-User-ID': userId, };
+    const getSongs = useApiService<ISong[]>("get", url, { params, initialData, headers }).fetchData;
+    return getSongs;
 }
