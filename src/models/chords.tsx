26c5b5b823b5b30d0dd89db6a75cd8b2b7baf168
Martin Johannes Nilsen <martin.johannes.nilsen@ciber.no>
2020-07-23 14:21:23 +0200

    La inn ekstra akkorder

--- src/models/chords.tsx
+++ src/models/chords.tsx
@@ -1,9 +1,28 @@
 export const chords = {
     "C-dur": ["C", "E", "G"],
+    "C#-dur": ["C#", "E#", "G#"],
+    "D-dur": ["D", "F#", "A"],
+    "D#-dur": ["D#", "G", "A#"],
+    "E-dur": ["E", "G#", "B"],
     "F-dur": ["F", "A", "C"],
+    "F#-dur": ["F#", "A#", "C#"],
     "G-dur": ["G", "H", "D"],
+    "G#-dur": ["G#", "C", "D#"],
+    "A-dur": ["A", "C#", "E"],
+    "A#-dur": ["A#", "D", "F"],
+    "H-dur": ["B", "D#", "F#"],
+
+    "C-moll": ["C", "D#", "G"],
+    "C#-moll": ["C#", "E", "G#"],
     "D-moll": ["D", "F", "A"],
+    "D#-moll": ["D#", "F#", "A#"],
     "E-moll": ["E", "G", "H"],
-    "A-moll": ["A", "C", "E"]
+    "F-moll": ["F", "G#", "C"],
+    "F#-moll": ["F#", "A", "C#"],
+    "G-moll": ["G", "A#", "D"],
+    "G#-moll": ["G#", "H", "D#"],
+    "A-moll": ["A", "C", "E"],
+    "A#-moll": ["A#", "C#", "F"],
+    "H-moll": ["B", "D", "F#"],
 }
 
