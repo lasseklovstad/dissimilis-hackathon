ddec5a7e8504965c98dfd9924f6ddd0d3605d5be
Martin Johannes Nilsen <martin.johannes.nilsen@ciber.no>
2020-07-24 13:44:38 +0200

    puller dev

--- src/utils/useLogin.tsx
+++ src/utils/useLogin.tsx
@@ -5,20 +5,19 @@ import { useApiService } from "./useApiService";
  * @param id songs id
  */
 
-//const url = "https://dissimilisfargenotasjon.azurewebsites.net"
-const url = "https://localhost:3000"
+const url = "https://dissimilisfargenotasjon.azurewebsites.net"
 
 
 export const useLoginRedirect = () => {
     const url = 'login';
-    const params = { "web_app_url": "https://dissimilisfargenotasjon.azurewebsites.net" };
+    const params = { "web_app_url": "https://localhost:3000" };
     const getLoginUrl = useApiService<string>("get", url, { params }).fetchData;
     return getLoginUrl;
 }
 
 export const useLoginPost = (code: string | null) => {
     const url = 'login';
-    const params = { "web_app_url": "https://dissimilisfargenotasjon.azurewebsites.net", "X-API-Key": sessionStorage.getItem("apiKey") || "", "X-User-ID": sessionStorage.getItem("userId") || "" };
+    const params = { "web_app_url": "https://localhost:3000", "X-API-Key": sessionStorage.getItem("apiKey") || "", "X-User-ID": sessionStorage.getItem("userId") || "" };
     const body = { "code": code };
     const postLogin = useApiService<Token>("post", url, { params, body }).postData;
     return postLogin;
