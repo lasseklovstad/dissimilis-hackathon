8734c51411946cd09d6a8002438909949973c46c
Martin.Johannes.Nilsen <martin.johannes.nilsen@ciber.no>
2020-07-26 15:45:20 +0200

    gjort noen endringer på akkordlista

--- src/components/BottomBar/BottomBar.component.tsx
+++ src/components/BottomBar/BottomBar.component.tsx
@@ -14,6 +14,8 @@ import { chords } from '../../models/chords';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import { SongToolsContext } from '../../views/SongView/SongToolsContextProvider.component';
+import { Chord } from '@tonaljs/tonal';
+import { getChord } from '../Bar/BarBody.component'
 
 const StyledToggleButtonGroup = withStyles((theme) => ({
     grouped: {
@@ -37,6 +39,9 @@ function BottomBar() {
     let chordArray: string[] = [];
     for(let i = 0; i < chords.length; i++){
         chordArray.push(chords[i].name);
+        //For adding new chords in the list, you can check if the given chords are showing the same name in the songView as it is shown in the dropdown, or if the chord are undefined or not (wrong name?)
+        //if(chords[i].name !== getChord(chords[i].notes)) console.log(chords[i].name + " = " + getChord(chords[i].notes));
+        //if(chords[i].name === undefined) console.log(chords[i].name + " = undefined ");
     }
     const { selectedNoteLength, setSelectedNoteLength } = useContext(SongToolsContext);
     const { addEmptyBar, getTimeSignature } = useContext(SongContext);
