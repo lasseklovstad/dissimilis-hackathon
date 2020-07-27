589b344441e20aea8ff927e874123019bda4acce
Knut Formo Buene <knut.formo.buene@ciber.no>
2020-07-27 08:47:41 +0200

    skrevet om testen for LoginView og skrevet enkelt innledende test til BottomBar

---
+++ src/components/BottomBar/BottomBar.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom";
+import LoginView from './BottomBar.component'
+import BottomBar from './BottomBar.component';
+
+describe('BottomBar', () => {
+    /* 
+        it('Autocomplete should be showing on bottombar', function () {
+            render(<BrowserRouter> <BottomBar /> </BrowserRouter >)
+            expect(screen.findAllByRole('textfield')).not.toBeDisabled
+        }) */
+
+    it('Add tone button should be showing on bottombar', function () {
+        render(<BrowserRouter> <BottomBar /> </BrowserRouter>);
+        const addToneButton = screen.getAllByRole("button", { name: /BottomBar:addTone/i })
+        expect(addToneButton[0]).toBeEnabled();
+
+    })
+    it('Add Bar button should be showing on login page', function () {
+        render(<BrowserRouter> <BottomBar /> </BrowserRouter>);
+        const addBarButton = screen.getAllByRole("button", { name: /BottomBar:addBar/i })
+        expect(addBarButton[0]).toBeEnabled();
+
+    })
+
+})
