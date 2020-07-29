5af31e16758b8211fdd5c482f302b0fe10bc817c
Martin Johannes Nilsen <martin.johannes.nilsen@ciber.no>
2020-07-29 09:19:53 +0200

    Fikset noe styling

--- src/components/Bar/BarBody.component.tsx
+++ src/components/Bar/BarBody.component.tsx
@@ -201,7 +201,7 @@ export const BarBody: React.FC<BarBodyProps> = props => {
                                             key={index} className={classes.toneBox}
                                             onMouseEnter={() => { if (showPossiblePositions) { setPositionArray(selectPositionArray(props.voiceId, props.barNumber, i)); } }}
                                             onMouseLeave={() => { if (showPossiblePositions) { setPositionArray([]) } }}
-                                            style={{ cursor: showPossiblePositions ? (!availablePositions[props.voiceId][props.barNumber].find(arr => arr.includes(i))) ? 'context-menu' : "pointer" : "default", backgroundColor: emptySpace(i) ? (positionArray.includes(i) ? colors.focus : "transparent") : getColor(type), outlineColor: "black", boxShadow: emptySpace(i) ? (positionArray.includes(i) ? "none" : "0 0 5px black") : "none", opacity: showPossiblePositions ? "80%" : "100%" }}
+                                            style={{ cursor: showPossiblePositions ? (!availablePositions[props.voiceId][props.barNumber].find(arr => arr.includes(i))) ? 'context-menu' : "pointer" : "default", backgroundColor: emptySpace(i) ? (positionArray.includes(i) ? colors.focus : "transparent") : getColor(type), border: emptySpace(i) ? (positionArray.includes(i) ? "none" : "1px solid" + colors.gray_400) : "none", opacity: (showPossiblePositions && !emptySpace(i)) ? "80%" : "100%" }}
                                             component={ButtonBase}
                                             onClick={() => {
                                                 if (showPossiblePositions && availablePositions[props.voiceId][props.barNumber].find(arr => arr.includes(i)) != null) {
