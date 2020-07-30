9ac7207ada177bcb6956fc8f3dd17e6710a2976a
Max Torre Schau <max.torre.schau@ciber.no>
2020-07-30 13:29:40 +0200

    Endret på struktur på komponenter

--- src/components/MenuButton/MenuButton.component.tsx
+++ src/components/MenuButton/MenuButton.component.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { makeStyles, IconButton, Menu, MenuItem, Switch } from "@material-ui/core";
+import { makeStyles, IconButton, Menu, MenuItem } from "@material-ui/core";
 import colors from "../../utils/colors";
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { useTranslation } from "react-i18next";
@@ -18,19 +18,24 @@ type MatchParams = {
 
 export const MenuButton: React.FC<MenuButtonProps> = props => {
     const classes = useStyles();
-    const history = useHistory();
-    const { song } = useContext(SongContext);
-    const putSong = usePutSong(song);
-    const { setShowPossiblePositions } = useContext(SongToolsContext);
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const { song } = useContext(SongContext);
+    const { setShowPossiblePositions } = useContext(SongToolsContext);
+
+    const { t } = useTranslation();
+    const history = useHistory();
+    const putSong = usePutSong(song);
+
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
     };
 
     const match = useRouteMatch<MatchParams>("/song/:id")
-    let id = match ? +match.params.id : 0
+    let id = match ? +match.params.id : 0;
+
     const handleClose = (method = "") => {
         setAnchorEl(null);
         switch (method) {
@@ -42,11 +47,9 @@ export const MenuButton: React.FC<MenuButtonProps> = props => {
                 putSong();
                 break;
             default:
-
         }
     };
 
-    const { t } = useTranslation();
 
     return (
         <div>
@@ -60,7 +63,6 @@ export const MenuButton: React.FC<MenuButtonProps> = props => {
                 <MenuItem onClick={() => handleClose("export")}>{t("MenuButton:export")}</MenuItem>
                 <MenuItem disabled onClick={() => handleClose("")}>{t("MenuButton:hide")}</MenuItem>
             </Menu>
-
         </div>
     );
 };
