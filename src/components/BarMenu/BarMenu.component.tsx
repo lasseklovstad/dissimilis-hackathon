8083b1aba0b77cbbfc009aa6969b1f4c57c0a2ef
Lasse Kløvstad <lasse.klovstad@ciber.no>
2021-01-13 21:24:59 +0100

    DISS-332; Rydde opp

--- src/components/BarMenu/BarMenu.component.tsx
+++ src/components/BarMenu/BarMenu.component.tsx
@@ -2,7 +2,11 @@ import React, { useContext } from "react"
 import { Menu, MenuItem } from "@material-ui/core"
 import { useTranslation } from "react-i18next"
 import { IBar } from "../../models/IBar"
-import {useDeleteBar, useDuplicateBar, useUpdateBar} from "../../utils/useApiServiceSongs"
+import {
+    useDeleteBar,
+    useDuplicateBar,
+    useUpdateBar,
+} from "../../utils/useApiServiceSongs"
 import { SongContext } from "../../views/SongView/SongContextProvider.component"
 
 type BarMenuProps = {
@@ -27,16 +31,16 @@ export const BarMenu = (props: BarMenuProps) => {
                 dispatchSong({ type: "DELETE_BAR", barPosition: bar.position })
             }
         }
-         if (method === "duplicate") {
-             const { error, result } = await duplicateBar.run({
-                 fromPosition: bar.position,
-                 copyLength: 1,
-                 toPosition: bar.position + 1
-             })
-             if (!error && result) {
-                 dispatchSong({ type: "UPDATE_SONG", song: result.data})
-             }
-         }
+        if (method === "duplicate") {
+            const { error, result } = await duplicateBar.run({
+                fromPosition: bar.position,
+                copyLength: 1,
+                toPosition: bar.position + 1,
+            })
+            if (!error && result) {
+                dispatchSong({ type: "UPDATE_SONG", song: result.data })
+            }
+        }
         if (method === "toggleRepBefore") {
             const { error, result } = await putBar.run({
                 repBefore: !bar.repBefore,
@@ -58,7 +62,6 @@ export const BarMenu = (props: BarMenuProps) => {
             }
         }
         if (method === "addHouseOne") {
-            console.log("Bar position is:", bar.position)
             const { error, result } = await putBar.run({
                 repBefore: bar.repBefore,
                 repAfter: bar.repAfter,
@@ -67,7 +70,6 @@ export const BarMenu = (props: BarMenuProps) => {
             if (!error && result) {
                 dispatchSong({ type: "UPDATE_VOICE", voice: result.data })
             }
-
         }
         if (method === "addHouseTwo") {
             const { error, result } = await putBar.run({
@@ -76,7 +78,6 @@ export const BarMenu = (props: BarMenuProps) => {
                 house: 2,
             })
             if (!error && result) {
-                console.log("addHouseTwo result", result.data)
                 dispatchSong({ type: "UPDATE_VOICE", voice: result.data })
             }
         }
@@ -119,20 +120,22 @@ export const BarMenu = (props: BarMenuProps) => {
             </MenuItem>
             <MenuItem
                 onClick={() =>
-                    handleClose(bar.house === 1 ? "removeHouseOne" : "addHouseOne")
+                    handleClose(
+                        bar.house === 1 ? "removeHouseOne" : "addHouseOne"
+                    )
                 }
             >
-
                 {bar.house === 1
                     ? t("BarContainer:removeHouseOne")
                     : t("BarContainer:addHouseOne")}
             </MenuItem>
             <MenuItem
                 onClick={() =>
-                    handleClose(bar.house === 2 ? "removeHouseTwo" : "addHouseTwo")
+                    handleClose(
+                        bar.house === 2 ? "removeHouseTwo" : "addHouseTwo"
+                    )
                 }
             >
-
                 {bar.house === 2
                     ? t("BarContainer:removeHouseTwo")
                     : t("BarContainer:addHouseTwo")}
