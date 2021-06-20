1f933fab04a5b5fc68633144a3d636b6f08aecb9
Lasse Kløvstad <lasse.klovstad@ciber.no>
2021-06-20 12:23:28 +0200

    integrasjonstester

--- src/views/LoginView/LoginView.test.tsx
+++ src/views/LoginView/LoginView.test.tsx
@@ -1,43 +1,85 @@
-/* eslint-disable func-names */
 import React from "react"
-import { render, screen } from "@testing-library/react"
-import { BrowserRouter } from "react-router-dom"
+import { render, screen, waitFor } from "@testing-library/react"
 import { LoginView } from "./LoginView"
+import { TestWrapper } from "../../TestWrapper.komponent"
+import userEvent from "@testing-library/user-event"
+import { setupServer } from "msw/node"
+import { rest } from "msw"
+import { Token } from "../../utils/useApiServiceLogin"
+import { sessionStorageMock } from "../../setupTests"
+
+const mockUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
+const mockReplaceHistory = jest.fn()
+
+jest.mock("react-router", () => ({
+    ...(jest.requireActual("react-router") as any),
+    useHistory: () => ({
+        replace: mockReplaceHistory,
+        push: jest.fn(),
+    }),
+}))
+
+const server = setupServer(
+    rest.get(/login/i, (req, res, ctx) => {
+        return res(ctx.status(204), ctx.set("location", mockUrl))
+    }),
+    rest.post<{ code: string }, Token>(/login/i, (req, res, ctx) => {
+        return res(
+            ctx.json({
+                apiKey: "36476d21801ece54d1f967d89c01cdd5",
+                userID: 5,
+            })
+        )
+    })
+)
 
 describe("LoginView", () => {
-    it("textfield should be showing on login page", function () {
-        render(
-            <BrowserRouter>
-                {" "}
-                <LoginView />{" "}
-            </BrowserRouter>
-        )
-        expect(screen.findAllByRole("textfield")).not.toBeDisabled()
+    let location: any
+    beforeAll(() => {
+        // Enable the mocking in tests.
+        server.listen({ onUnhandledRequest: "warn" })
     })
 
-    it("LoginButton should be showing on login page", function () {
-        render(
-            <BrowserRouter>
-                {" "}
-                <LoginView />{" "}
-            </BrowserRouter>
-        )
-        const LoginButton = screen.getAllByRole("button", {
-            name: /LoginView.login/i,
-        })
-        expect(LoginButton[0]).toBeEnabled()
+    afterEach(() => {
+        // Reset any runtime handlers tests may use.
+        window.location = location
+        server.resetHandlers()
     })
-    it("LoginButton for Microsoft should be showing on login page", function () {
-        render(
-            <BrowserRouter>
-                {" "}
-                <LoginView />{" "}
-            </BrowserRouter>
+
+    afterAll(() => {
+        // Clean up once the tests are done.
+        server.close()
+    })
+    beforeEach(() => {
+        location = window.location
+        delete window.location
+        window.location = new URL(location.href) as any
+        window.location.assign = jest.fn()
+        window.location.replace = jest.fn()
+        window.location.reload = jest.fn()
+        sessionStorageMock.clear()
+    })
+
+    it("should login user and redirect", async () => {
+        render(<LoginView />, { wrapper: TestWrapper })
+        userEvent.click(
+            screen.getByRole("button", {
+                name: /Logg inn med microsoft/i,
+            })
         )
-        const LoginButtonMicrosoft = screen.getAllByRole("button", {
-            name: /LoginView.loginWithMicrosoft/i,
+        await waitFor(() => expect(window.location.href).toBe(mockUrl))
+    })
+
+    it("should login user and set sessionStorage items", async () => {
+        window.location.search = "code=abc-123"
+        render(<LoginView />, { wrapper: TestWrapper })
+        await waitFor(() => {
+            expect(mockReplaceHistory).toHaveBeenCalledWith("/dashboard")
         })
-        expect(LoginButtonMicrosoft[0]).toBeEnabled()
+        expect(sessionStorageMock.getItem("apiKey")).toBe(
+            "36476d21801ece54d1f967d89c01cdd5"
+        )
+        expect(sessionStorageMock.getItem("userId")).toBe("5")
     })
 
     it("should route user to /dashboard if already logged inn", () => {
