c9a5cdf0e8549ac161dfd99db8d3e92d9e70e705
Lasse Kløvstad <lasse.klovstad@ciber.no>
2022-02-23 22:03:40 +0100

    DISS-729 Aria label for chords

--- src/components/Chord/ChordAsButton.component.tsx
+++ src/components/Chord/ChordAsButton.component.tsx
@@ -4,6 +4,7 @@ import { getNoteText } from "./Chord.util"
 import { Note } from "./Note.component"
 import { ChordButton } from "./ChordButton.component"
 import { IChord } from "../../models/IChord"
+import { useTranslation } from "react-i18next"
 
 type ChordAsButtonProps = {
     ButtonProps: Pick<
@@ -23,10 +24,24 @@ type ChordAsButtonProps = {
 
 export const ChordAsButton = (props: ChordAsButtonProps) => {
     const { ButtonProps, chord, showNoteLetters, isSelected, highlight } = props
+    const { t } = useTranslation()
+    const isEmpty = chord.notes[0] === "Z"
+
+    const getAriaLabel = () => {
+        if (isEmpty) {
+            return t("Song.emptyChord")
+        } else {
+            return chord.chordName
+                ? `${t("Song.chordLabel")} ${chord.chordName}`
+                : `${t("Song.noteLabel")} ${chord.notes[0]}`
+        }
+    }
+
     return (
         <ChordButton
-            emptyChord={chord.notes[0] === "Z"}
+            emptyChord={isEmpty}
             aria-selected={isSelected}
+            aria-label={getAriaLabel()}
             id="chordButton"
             disableRipple
             {...ButtonProps}
